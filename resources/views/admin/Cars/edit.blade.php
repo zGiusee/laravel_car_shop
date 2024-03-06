b1d9b49e1279def3550ad4793d12763dc93865c7
moonymel <musumeci.me@gmail.com>
2024-03-06 12:53:09 +0100

    [UPDATE] updated store&update brand requests, create & edit of brands and cars

--- resources/views/admin/Cars/edit.blade.php
+++ resources/views/admin/Cars/edit.blade.php
@@ -3,13 +3,28 @@
 @section('content')
     <div class="container">
         <div class="row">
+
+            {{-- DISPLAY SUMMIT DI ERRORI  --}}
+            <div class="col-6">
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                        <li>{{ $error }} </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            </div>
+
+            {{-- INIZIO FORM  --}}
             <div class="col-12 mt-3">
                 <form action="{{ route('admin.cars.update', ['car' => $car->id]) }}" method="post">
                     @csrf
                     @method('PUT')
                     <div class="form-group mt-3">
                         <label for="price" class="control-label">Price:</label>
-                        <input required type="text" name="price" id="price" placeholder="Price"
+                        <input required type="text" name="price" id="price"
                             class="form-control @error('price') is-invalid @enderror" value="{{ old('price') }}">
                         @error('price')
                             <div class="text-danger">{{ $message }}</div>
@@ -17,31 +32,31 @@
                     </div>
                     <div class="form-group mt-3">
                         <label for="model">Model:</label>
-                        <input required type="text" name="model" id="model" placeholder="Model"
+                        <input required type="text" name="model" id="model"
                             class="form-control @error('model') is-invalid @enderror" value="{{ old('model') }}">
                         @error('model')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="form-group mt-3">
-                        <label for="power">power:</label>
-                        <input required type="text" name="power" id="power" placeholder="power"
+                        <label for="power">Power:</label>
+                        <input required type="text" name="power" id="power"
                             class="form-control @error('power') is-invalid @enderror" value="{{ old('power') }}">
                         @error('power')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="form-group mt-3">
-                        <label for="year">year:</label>
-                        <input required type="text" name="year" id="year" placeholder="year"
+                        <label for="year">Year:</label>
+                        <input required type="text" name="year" id="year"
                             class="form-control @error('year') is-invalid @enderror" value="{{ old('year') }}">
                         @error('year')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="form-group mt-3">
-                        <label for="color">color:</label>
-                        <input required type="text" name="color" id="color" placeholder="color"
+                        <label for="color">Color:</label>
+                        <input required type="text" name="color" id="color"
                             class="form-control @error('color') is-invalid @enderror" value="{{ old('color') }}">
                         @error('color')
                             <div class="text-danger">{{ $message }}</div>
@@ -49,8 +64,8 @@
                     </div>
 
                     <div class="form-group mt-3">
-                        <label for="fuel_type">fuel_type:</label>
-                        <input required type="text" name="fuel_type" id="fuel_type" placeholder="fuel_type"
+                        <label for="fuel_type">Fuel Type:</label>
+                        <input required type="text" name="fuel_type" id="fuel_type"
                             class="form-control @error('fuel_type') is-invalid @enderror" value="{{ old('fuel_type') }}">
                         @error('fuel_type')
                             <div class="text-danger">{{ $message }}</div>
@@ -58,8 +73,8 @@
                     </div>
 
                     <div class="form-group mt-3">
-                        <label for="used">used:</label>
-                        <input required type="text" name="used" id="used" placeholder="used"
+                        <label for="used">Used:</label>
+                        <input required type="text" name="used" id="used"
                             class="form-control @error('used') is-invalid @enderror" value="{{ old('used') }}">
                         @error('used')
                             <div class="text-danger">{{ $message }}</div>
@@ -87,8 +102,8 @@
                     </div>
 
                     <div class="form-group mt-3">
-                        <label for="seats">seats:</label>
-                        <input required type="text" name="seats" id="seats" placeholder="seats"
+                        <label for="seats">Seats:</label>
+                        <input required type="text" name="seats" id="seats"
                             class="form-control @error('seats') is-invalid @enderror" value="{{ old('seats') }}">
                         @error('seats')
                             <div class="text-danger">{{ $message }}</div>
@@ -96,8 +111,8 @@
                     </div>
 
                     <div class="form-group mt-3">
-                        <label for="transmission">transmission:</label>
-                        <input required type="text" name="transmission" id="transmission" placeholder="transmission"
+                        <label for="transmission">Transmission:</label>
+                        <input required type="text" name="transmission" id="transmission"
                             class="form-control @error('transmission') is-invalid @enderror"
                             value="{{ old('transmission') }}">
                         @error('transmission')
@@ -106,8 +121,8 @@
                     </div>
 
                     <div class="form-group mt-3">
-                        <label for="engine_size">engine_size:</label>
-                        <input required type="text" name="engine_size" id="engine_size" placeholder="engine_size"
+                        <label for="engine_size">Engine Size:</label>
+                        <input required type="text" name="engine_size" id="engine_size"
                             class="form-control @error('engine_size') is-invalid @enderror"
                             value="{{ old('engine_size') }}">
                         @error('engine_size')
@@ -115,6 +130,7 @@
                         @enderror
                     </div>
 
+                    {{-- BOTTONE DI INVIO  --}}
                     <div class="form-group mt-3">
                         <button type="submit" class="btn btn-sm btn-primary">Save</button>
                     </div>
