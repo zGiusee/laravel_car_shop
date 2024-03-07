5cf8ea1681a472be18291ed954429fe06df67f7f
zGiusee <giusebelforte@gmail.com>
2024-03-07 14:20:36 +0100

    [UPDATE] Modified style

--- resources/views/admin/Cars/show.blade.php
+++ resources/views/admin/Cars/show.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="bg-dark p-5 mt-4">
+    <div class="p-5 mt-4">
         <main class="bg-white pb-5">
             <div class="container py-3">
                 <div class="row">
                     <div class="col-7">
                         <div class="image_container">
-                            <img src="https://www.mgcars.com/images/mg-models/newmghsphev/color/Black.png" alt="image_car"
-                                class="img-fluid">
+                            @if ($car->img !== null)
+                                @if (Str::contains($car->img, 'https'))
+                                    <img src="{{ $car->img }}" alt="{{ $car->name }}">
+                                @else
+                                    <img src="{{ asset('/storage/' . $car->img) }}" alt="{{ $car->name }}">
+                                @endif
+                            @else
+                                <img src="{{ asset('/storage/cars_image/carplaceholder.png') }}" alt="{{ $car->name }}">
+                            @endif
                         </div>
 
                         {{-- ICONE SOTTO IMMAGINE --}}
@@ -64,96 +71,60 @@
                             </div>
                         </div>
                     </div>
-
-                    {{-- LISTA ICONE --}}
                     <div class="col-5">
-                        <ul class="list-unstyled d-flex text-primary">
-                            <li class="me-5">
-                                <span><i class="fa-regular fa-star"></i></span>
-                                <span>Save</span>
-                            </li>
-                            <li class="me-5">
-                                <i class="fa-solid fa-share-nodes"></i>
-                                <span>Share</span>
-                            </li>
-                            <li class="me-5">
-                                <i class="fa-solid fa-print"></i>
-                                <span>Print</span>
-                            </li>
-                        </ul>
 
+                        <h2 class="fw-bolder">More Details</h2>
                         {{-- NOME MODELLO --}}
                         <div class="fw-bolder fs-4 my-3">{{ $car->model }}</div>
-
-                        {{-- LOCATION --}}
-                        <div class="text-primary pb-4 border-bottom">
-                            <span class="me-2"><i class="fa-solid fa-location-dot"></i></span>
-                            <span>Milan, IT</span>
-                        </div>
 
                         {{-- PRICE --}}
                         <div class="my-5 fs-3">{{ number_format($car->price, 2, ',', '.') . ' €' }}</div>
 
-                        {{-- CONTACT --}}
-                        <div class="pb-4 border-bottom">
-                            <a class="btn btn-primary me-2 fw-bolder text-uppercase px-4">Contact Owner</a>
-                            <a class="btn btn-primary me-2 fw-bolder text-uppercase px-4">+393194256877</a>
-                        </div>
+                        <ul class="list-unstyled">
+                            <li class="my-2"><span class="text-uppercase fw-bolder ">Immatricolation year:</span>
+                                <span
+                                    class="fst-italic">{{ $car->immatricolation_year != null ? $car->immatricolation_year : 'Not available' }}</span>
+                            </li>
+
+                            <li class="my-2"><span class="text-uppercase fw-bolder">Used:</span>
+                                <span class="fst-italic">{{ $car->used != false ? 'Yes' : 'No' }}</span>
+                            </li>
+
+                            <li class="my-2"><span class="text-uppercase fw-bolder">Plate:</span>
+                                <span class="fst-italic">{{ $car->plate }}</span>
+                            </li>
+
+                            <li class="my-2"><span class="text-uppercase fw-bolder">Color:</span>
+                                <span class="fst-italic">{{ $car->color }}</span>
+                            </li>
+
+                            <li class="my-2"><span class="text-uppercase fw-bolder">Paynt type:</span>
+                                <span
+                                    class="fst-italic">{{ $car->paint_type != null ? $car->paint_type : 'Not available' }}</span>
+                            </li>
+
+                            <li class="my-2"><span class="text-uppercase fw-bolder">Material type</span>
+                                <span
+                                    class="fst-italic">{{ $car->material_type != null ? $car->material_type : 'Not available' }}</span>
+                            </li>
+
+                            <li class="my-2"><span class="text-uppercase fw-bolder">Number of seats:</span>
+                                <span class="fst-italic">{{ $car->seats }}</span>
+                            </li>
+
+                            <li class="my-2"><span class="text-uppercase fw-bolder">Description</span>
+                                <span
+                                    class="fst-italic">{{ $car->description != null ? $car->description : 'Not available' }}</span>
+                            </li>
+
+                            <li class="my-2"><span class="text-uppercase fw-bolder">Engine size:</span>
+                                <span class="fst-italic">{{ $car->engine_size }}</span>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
 
-            <div class="container border-top border-bottom">
-                <div class="row">
-                    <div class="col-12">
-                        <h2 class="fw-bolder">Moore Details</h2>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <ul class="list-unstyled">
-                        <li class="my-2"><span class="text-uppercase fw-bolder ">Immatricolation year:</span>
-                            <span
-                                class="fst-italic">{{ $car->immatricolation_year != null ? $car->immatricolation_year : 'Not available' }}</span>
-                        </li>
-
-                        <li class="my-2"><span class="text-uppercase fw-bolder">Used:</span>
-                            <span class="fst-italic">{{ $car->used != false ? 'Yes' : 'No' }}</span>
-                        </li>
-
-                        <li class="my-2"><span class="text-uppercase fw-bolder">Plate:</span>
-                            <span class="fst-italic">{{ $car->plate }}</span>
-                        </li>
-
-                        <li class="my-2"><span class="text-uppercase fw-bolder">Color:</span>
-                            <span class="fst-italic">{{ $car->color }}</span>
-                        </li>
-
-                        <li class="my-2"><span class="text-uppercase fw-bolder">Paynt type:</span>
-                            <span
-                                class="fst-italic">{{ $car->paint_type != null ? $car->paint_type : 'Not available' }}</span>
-                        </li>
-
-                        <li class="my-2"><span class="text-uppercase fw-bolder">Material type</span>
-                            <span
-                                class="fst-italic">{{ $car->material_type != null ? $car->material_type : 'Not available' }}</span>
-                        </li>
-
-                        <li class="my-2"><span class="text-uppercase fw-bolder">Number of seats:</span>
-                            <span class="fst-italic">{{ $car->seats }}</span>
-                        </li>
-
-                        <li class="my-2"><span class="text-uppercase fw-bolder">Description</span>
-                            <span
-                                class="fst-italic">{{ $car->description != null ? $car->description : 'Not available' }}</span>
-                        </li>
-
-                        <li class="my-2"><span class="text-uppercase fw-bolder">Engine size:</span>
-                            <span class="fst-italic">{{ $car->engine_size }}</span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
         </main>
     </div>
 @endsection
